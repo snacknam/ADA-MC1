70ce4d4b014ad245868dd003bb35581f2ae3d821
snack <kwansiknam@gmail.com>
2023-04-03 23:41:29 +0900

    add MC1 project

---
+++ MC1/MC1_Project.swiftpm/CardData.swift
@@ -0,0 +1,14 @@
+//
+//  Data.swift
+//  MC1_Project
+//
+//  Created by 관식 on 2023/03/30.
+//
+
+import Foundation
+
+struct CardData {
+    var cardEmoji: String
+    var cardTitle: String
+    var cardDescription: String
+}
