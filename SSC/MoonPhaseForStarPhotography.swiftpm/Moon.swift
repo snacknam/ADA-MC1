c5fe54e383cfe2ed523f6b9447b397e9974ad290
snack <kwansiknam@gmail.com>
2023-04-05 15:18:54 +0900

    add SSC project

---
+++ SSC/MoonPhaseForStarPhotography.swiftpm/Moon.swift
@@ -0,0 +1,13 @@
+//
+//  Moon.swift
+//  MoonPhaseForStarPhotography
+//
+//  Created by 관식 on 2023/04/05.
+//
+
+import Foundation
+
+struct Moon {
+    let title: String
+    let description: String
+}
---
+++ SSC/MoonPhaseForStarPhotography.swiftpm/MoonData.swift
@@ -0,0 +1,56 @@
+//
+//  MoonData.swift
+//  MoonPhaseForStarPhotography
+//
+//  Created by 관식 on 2023/04/05.
+//
+
+import Foundation
+
+extension Date {
+    static let dateFormatter: DateFormatter = {
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "MM/dd/yyyy"
+        return dateFormatter
+    }()
+    
+    init(_ text: String) {
+        self = Self.dateFormatter.date(from: text)!
+    }
+}
+
+struct MoonData {
+    
+    mutating func moonPhase() -> Int {
+        let lunarPhaseStart = Date("01/07/1970")
+        let daysSinceStart = Calendar.current.dateComponents(
+            [.day],
+            from: lunarPhaseStart,
+            to: Date()
+        ).day!
+
+        let seconds = daysSinceStart * 86400 + 12300
+        let lunarMonths = seconds % 2551443
+        let lunarMonthPart = lunarMonths / 637861
+        let secondsSinceMainPhase = lunarMonths % 637861
+
+        let index = 2 * lunarMonthPart + (86400 <= secondsSinceMainPhase ? 1 : 0)
+
+//        let lunarPhases = ["🌑", "🌒", "🌓", "🌔", "🌕", "🌖", "🌗", "🌘"]
+//        let lunarPhase = lunarPhases[index]
+
+        return index
+    }
+    
+    let moonExpression: [Moon] = [
+        Moon(title: "new moon", description: "today is new moon."),
+        Moon(title: "waxing crescent", description: "today is waxing crescent."),
+        Moon(title: "first quater", description: "today is first quater."),
+        Moon(title: "waxing gibbous", description: "today is waxing gibbous."),
+        Moon(title: "full moon", description: "today is full moon."),
+        Moon(title: "waning gibbous", description: "today is waning gibbous."),
+        Moon(title: "last quarter", description: "today is last quarter"),
+        Moon(title: "waning crescent", description: "today is waning crescent."),
+    ]
+    
+}
